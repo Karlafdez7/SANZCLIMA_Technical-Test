d3491241f10b094cd0f96380c43fa130b6dcfdf7
Karla <karlafernandezchirinos@gmail.com>
2023-06-14 00:34:16 +0200

    add History_Basic-Structure-Html_Function

--- web-sanzclim/app/history/page.tsx
+++ web-sanzclim/app/history/page.tsx
@@ -1,7 +1,18 @@
 export default function History (){
+    //¿Qué variables necesito? necesito la variable de estado donde se recoge la operación
+    //Get del LocalStorage para pintar los cálculos hechos en '/calc'
+
+    //Estructura básica de HTML para saber qué funciones o variables de estados necesito
     return(
-        <h1>Donde se guardan los cálculos</h1>
+        <div>
+            <h1>Donde se guardan los cálculos</h1>
+            <section>
+                <p>Números de la suma</p>
+                <p>Resultado de la suma</p>
+            </section>
+        </div>
+        
+        
     )
 }
-
 

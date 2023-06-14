1ccb80ebea09faab032186fa441f547830198aee
Karla <karlafernandezchirinos@gmail.com>
2023-06-14 16:27:20 +0200

    add Function-Add-Number__HTML-Butto-addNumber__List-Number-Calculation

--- web-sanzclim/app/calc/page.tsx
+++ web-sanzclim/app/calc/page.tsx
@@ -11,17 +11,25 @@ export default function Calculator (){
     const handleUserNumber = (event) => {
         setCurrentNumber(event.target.value);
     }
-    //Función que calcule la suma de los números que la usuaria recoja
-
+    //Función que calcule la suma de los números que la usuaria escriba. Una vez se añade el número, se vuelve a poner en blanco para añadir un nuevo número. 
+    //En HTML se ha puesto el type text, ya que en el ejercicio ofrecido por la empresa se especifica que no solo va a recibirse types number. 
+    const handleAddNumber = () => {
+        setNumbers([...numbers, parseFloat(currentNumber)]);
+        setCurrentNumber('')
+    }
     //Funcion para enviar para el LocalStorage
 
     //Estructura básica de HTML para saber las funciones y estados necesarios
     return (
         <div>
             <h1>Hola soy la calculadora</h1>
-            <input type='number' value={currentNumber} onChange={handleUserNumber}></input>
-            <button>Añadir número</button>
-            <ul>Lista de números que se añaden</ul>
+            <input type='text' value={currentNumber} onChange={handleUserNumber}></input>
+            <button onClick={handleAddNumber}>Añadir número</button>
+            <ul>Lista de números que se añaden
+                {numbers.map((number, index) => (
+                    <li key={index}>{number}</li>
+                ))}
+            </ul>
             <button>Sumar</button>
             <button>Guardar operación</button>
             <h2>Resultado del cálculo</h2>

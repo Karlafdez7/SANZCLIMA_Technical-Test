fe711d4f9ba453e06d0d3d273a3605e5ab3e6413
Karla <karlafernandezchirinos@gmail.com>
2023-06-15 18:16:15 +0200

    add HTML-CSS_Footer-responsive

---
+++ web-sanzclim/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import style from './footer.module.css'
+
+export function Footer(){
+    return (
+        <footer className={style.footer}>
+            <p className={style.copy}>©Karla's project Sanz Clim</p>
+        </footer>
+    )
+}

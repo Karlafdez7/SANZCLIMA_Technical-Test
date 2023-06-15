2df850324fb0d3d2ebf49c9b7d9aa8d912ed64c6
Karla <karlafernandezchirinos@gmail.com>
2023-06-15 16:48:06 +0200

    add HTML-CSS_Body-HomePages-responsive

--- web-sanzclim/app/page.tsx
+++ web-sanzclim/app/page.tsx
@@ -4,7 +4,11 @@ import './globals.css'
 export default function HomePage() {
 
   return(
-    <h1 className=''>Hola, soy un proyecto en Next.js</h1>
+    <section className='body_home'>
+      <h2 className='title'>Suma todo lo que quieras y recupera tus calculos guardados</h2>
+      <h3 className='sub_title'> Mi primer proyecto con Next.js y TypeScript</h3>
+    </section>
+      
   )
 }
 
